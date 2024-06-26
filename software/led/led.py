02840174465709fc911e57072968d8dbe7c2ab9e
johannes-bartl <johannes.bartl@fau.de>
2024-06-26 13:10:52 +0200

    untracked changes on pi

--- software/led/led.py
+++ software/led/led.py
@@ -4,13 +4,29 @@ import neopixel
 import sys
 
 n_leds = 30
+# j = 10
 pixels = neopixel.NeoPixel(board.D18,n_leds,brightness=1, auto_write=False)
 if sys.argv[1] == "on":
+    # for j in range(n_leds):
+        # pixels[j] = (255,255,255)
     pixels.fill((255, 255, 255))
     pixels.show()
 if sys.argv[1] == 'off':
+    # for j in range(n_leds):
+    #     pixels[j] = (0, 0, 0)
     pixels.fill((0,0,0))
     pixels.show()
+    time.sleep(0.1)  # Add a small delay to ensure the LEDs are updated
+    # pixels.deinit()
+
+    pixels.fill((0, 0, 0))
+    pixels.show()
+    time.sleep(0.1)  # Add a small delay to ensure the LEDs are updated
+
+
+    pixels.fill((0,0,0))
+    pixels.show()
+    time.sleep(0.1)  # Add a small delay to ensure the LEDs are updated
     pixels.deinit()
 
 # for i,c,c2 in zip(range(n_leds),range(0,255,255//30),range(0,255,255//30)[::-1]):
